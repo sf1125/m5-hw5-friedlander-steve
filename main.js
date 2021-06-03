d5f9808b7546ba5d4116a96961cd612cbe401642
Steve Friedlander <s.friedlander@ufl.edu>
2021-06-03 17:51:10 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var pizza = {
+    crust: "medium-crispy",
+    cheese: "mozzarella",
+    sauce: "tomato",
+    addToppings: "Please add pepperoni and sausage"
+};
+
+
+console.log(addToppings.valueOf());
+
