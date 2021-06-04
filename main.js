e00cc1e9f12dc41fb046de3392fcf8dc85302b25
Steve Friedlander <s.friedlander@ufl.edu>
2021-06-04 19:40:43 -0400

    Correct function code

--- main.js
+++ main.js
@@ -8,18 +8,35 @@ var pizza = {
 };
 
 function orderPizza(pizza){
-    console.log(crust);
-    console.log(mozzarella);
-    console.log(tomato);
-    console.log(addToppings);
-    addToppings: function() {
-        console.log("Please add pepperoni and sausage");
-    }
+    console.log(pizza.crust);
+    console.log(pizza.cheese);
+    console.log(pizza.sauce);
+    pizza.addToppings();
 }
 
-console.log(pizza);
-pizza.addToppings();
+orderPizza(pizza);
 
+
+
+// var a = 5;
+// var b = 8;
+
+// function example(x, y) {
+//     return x + y;
+// }
+
+// var z = example(a, b);
+
+// console.log(z);
+
+// var c = 2;
+// var d = 9;
+// var e = example(c, d);
+
+// console.log(e);
+
+// var f = example(3, 7);
+// console.log(f);
 
 
 
