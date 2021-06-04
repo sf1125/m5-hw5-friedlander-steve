7ce3f9cf97e577228c8654dc2705d3bdfe9eed37
Steve Friedlander <s.friedlander@ufl.edu>
2021-06-04 13:14:31 -0400

    Add method and function

--- main.js
+++ main.js
@@ -2,9 +2,26 @@ var pizza = {
     crust: "medium-crispy",
     cheese: "mozzarella",
     sauce: "tomato",
-    addToppings: "Please add pepperoni and sausage"
+    addToppings: function() {
+        console.log("Please add pepperoni and sausage");
+    }
 };
 
+function orderPizza(pizza){
+    console.log(crust);
+    console.log(mozzarella);
+    console.log(tomato);
+    console.log(addToppings);
+    addToppings: function() {
+        console.log("Please add pepperoni and sausage");
+    }
+}
 
-console.log(addToppings.valueOf());
+console.log(pizza);
+pizza.addToppings();
+
+
+
+
+
 
